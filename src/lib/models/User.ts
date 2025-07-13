dcf996fa8938e6be984bf406ef27924f725a9b84
desivar <jillian_desire@yahoo.es>
2025-07-13 05:27:29 -0600

    add User

---
+++ src/lib/models/User.ts
@@ -0,0 +1,10 @@
+// src/lib/models/User.ts
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+});
+
+export const User = model("User", userSchema);
