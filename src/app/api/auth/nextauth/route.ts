7a9e3c15b19f433daa3845ff6d4c9416cab55464
desivar <jillian_desire@yahoo.es>
2025-08-24 05:24:01 -0600

    updated router

--- src/app/api/auth/nextauth/route.ts
+++ src/app/api/auth/nextauth/route.ts
@@ -1,9 +1,10 @@
+
 // src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import { authOptions } from "@/auth"; // <-- This is what you had, and it is correct for a named export
+import { authOptions } from "@/auth"; // <-- Change this line
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
 
-export const runtime = "nodejs"; // <-- This line fixes the 'crypto' error
+export const runtime = "nodejs";
